c71158190e875053a03dc582da568e122066e8d7
Ruth John <rumyra@gmail.com>
2017-06-08 00:36:43 +0100

    slides are ok

---
+++ public/scripts/talkEvents.js
@@ -0,0 +1,63 @@
+// initialise Reveal
+Reveal.initialize({
+
+  // The "normal" size of the presentation, aspect ratio will be preserved
+  // when the presentation is scaled to fit different resolutions. Can be
+  // specified using percentage units.
+  width: 1024,
+  height: 768,
+
+  // Factor of the display size that should remain empty around the content
+  margin: 0.1,
+
+  // Bounds for smallest/largest possible scale to apply to content
+  minScale: 0.2,
+  maxScale: 1.5,
+
+  center: false,
+
+  // Display controls in the bottom right corner
+  controls: false,
+
+  // Display a presentation progress bar
+  progress: false,
+
+  // Display the page number of the current slide
+  slideNumber: false,
+
+  // Push each slide change to the browser history
+  history: true,
+
+  // Enable keyboard shortcuts for navigation
+  keyboard: {
+    25: 'next' // go to the next slide when the DOWN key is pressed
+  },
+
+  // Transition style
+  transition: 'fade', // none/fade/slide/convex/concave/zoom
+
+  // Transition speed
+  transitionSpeed: 'default', // default/fast/slow
+
+  // Transition style for full page slide backgrounds
+  backgroundTransition: 'default', // none/fade/slide/convex/concave/zoom
+
+  // Number of slides away from the current that are visible
+  viewDistance: 2,
+
+  // Parallax background image
+  parallaxBackgroundImage: '', // e.g. "'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg'"
+
+  // Parallax background size
+  parallaxBackgroundSize: '', // CSS syntax, e.g. "2100px 900px"
+
+  // Amount to move parallax background (horizontal and vertical) on slide change
+  // Number, e.g. 100
+  parallaxBackgroundHorizontal: '',
+  parallaxBackgroundVertical: ''
+
+});
+
+var svg = d3.select('#wave-graph')
+  .append('circle')
+  .attr('r', 20);
